11bad5891f7be40d726093dece42d65be835cee8
Aastha Mishra <aasthamishrafsdev@gmail.com>
2024-01-21 07:05:54 +0000

    typeconversion

--- Basics/04_Day2_TypeConversion.js
+++ Basics/04_Day2_TypeConversion.js
@@ -39,3 +39,34 @@ console.log(typeof price1);
 //any value type can be converted into a string
 
 /********************************************OPERATIONS************************************************ */
+
+let val = 2
+let negVal = -val //will give the negaitive value
+console.log(negVal)
+
+//Airthematic opertaions
+
+// val = 2 + 3 //addition
+// console.log(val)
+// val = 7 - 4 //substraction
+// console.log(val)
+// val = 5 * 1 //multiplication
+// console.log(val)
+// val = 2 ** 2 // raised to the power
+// console.log(val)
+// val = 12 / 4
+// console.log(val)
+// val = 17 % 5
+// console.log(val)
+
+let str1 = 1;
+let str2 = "2"
+console.log(str1 + str2); //this will convert the type of str1 from number to string and will concatenate it to str2. conversion depends on on the datatype of 1st variable if more than two variables are being added
+
+console.log("1"+ 2 + 3)//it will be converted into a string
+console.log(1+ 2 + "3") // it will be converted into a number
+
+console.log(+true)//will convert the boolean to number
+//console.log(true+) //will throw an error
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
